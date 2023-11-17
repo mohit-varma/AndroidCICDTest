8487148e377f430c8cbf282759e89d64dcf16295
mohit-varma <Mohit.Varma@viriminfotech.com>
2023-11-17 14:26:09 +0530

    retrofit add for fetch quotes detail.

--- app/src/main/java/com/example/androidcicdtest/MainActivity.kt
+++ app/src/main/java/com/example/androidcicdtest/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.androidcicdtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.example.androidcicdtest.interfaces.QuotesApi
+import kotlinx.coroutines.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textViewNameId : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val dexOutputDir: File = codeCacheDir
+        dexOutputDir.setReadOnly()
+        initViews()
+
+        var quotesApi = RetrofitHelper.getInstance().create(QuotesApi::class.java)
+        GlobalScope.launch(Dispatchers.IO) {
+           val quotesList =  quotesApi.getQuotes(page = 107)
+            withContext(Dispatchers.Main) {
+                textViewNameId.text = quotesList.body().toString()
+                quotesList.body()?.let {
+                    Log.d(MainActivity::class.java.simpleName, "onCreate: size = ${it.results.size}")
+                }
+            }
+        }
+    }
+
+    private fun initViews() {
+        textViewNameId = findViewById(R.id.textViewNameId)
     }
 }
